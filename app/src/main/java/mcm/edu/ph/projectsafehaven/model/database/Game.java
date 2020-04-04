0c740068580ada8e9159cc59f8b2a008c941ca20
Kiboot <kiboot@users.noreply.github.com>
2020-04-05 01:56:04 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/projectsafehaven/model/database/Game.java
@@ -0,0 +1,19 @@
+package mcm.edu.ph.projectsafehaven.model.database;
+
+public class Game {
+
+    public Game(){} //default constructor. put in place to prevent minor usage errors.
+
+    public static String TABLE_NAME = "persons";
+    public static String COLUMN_ID = "pid";
+    public static String COLUMN_FNAME = "fname";
+    public static String COLUMN_LNAME = "lname";
+    public static String COLUMN_JOB = "job";
+    public static String COLUMN_MONEY = "money";
+    public static String COLUMN_FOOD = "food";
+    public static String COLUMN_TRAITS = "traits";
+    public static String COLUMN_STRESS = "stress";
+
+
+
+}
---
+++ app/src/main/java/mcm/edu/ph/projectsafehaven/view/MainActivity.java
@@ -0,0 +1,18 @@
+package mcm.edu.ph.projectsafehaven.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mcm.edu.ph.projectsafehaven.R; //for more info https://www.javatpoint.com/R-java-file-in-android
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
