fef356362afa813059f323ea3ff0ebdd68c7dc1a
Kiboot <kiboot@users.noreply.github.com>
2020-04-12 01:44:33 +0800

    03:18 update: + Added Activity for Character picker. UI Design not final.

---
+++ app/src/main/java/mcm/edu/ph/projectsafehaven/view/CharPicker.java
@@ -0,0 +1,15 @@
+package mcm.edu.ph.projectsafehaven.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import mcm.edu.ph.projectsafehaven.R;
+
+public class CharPicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_char_picker);
+    }
+}
